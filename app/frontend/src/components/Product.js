c1460d85f6d35fd3ad530f87468b74088851630e
Efren Layan <efren.layan4@gmail.com>
2022-08-21 14:06:14 +0800

    Added app folder and moving frontend

---
+++ app/frontend/src/components/Product.js
@@ -0,0 +1,51 @@
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Rating from "./Rating";
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Store } from "../Store";
+
+function Product(props) {
+  const { product } = props;
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { cartItems },
+  } = state;
+
+  const [disabler, disable] = useState(false);
+
+  const addToCartHandler = async (item) => {
+    const existItem = cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      disable(true);
+      return;
+    }
+    ctxDispatch({
+      type: "CART_ADD_ITEM",
+      payload: { ...item, quantity },
+    });
+  };
+  return (
+    <Card className="product">
+      <Link to={`/product/${product.slug}`}>
+        <img src={product.image} className="card-img-top" alt={product.name} />
+      </Link>
+      <Card.Body>
+        <Link to={`/product/${product.slug}`}>
+          <Card.Title>{product.name}</Card.Title>
+        </Link>
+        <Rating rating={product.rating} numReviews={product.numReviews} />
+        <Card.Text>${product.price}</Card.Text>
+
+        <Button disabled={disabler} onClick={() => addToCartHandler(product)}>
+          Add to Cart
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Product;
---
+++ app/frontend/src/screens/PaymentMethodScreen.js
@@ -0,0 +1,72 @@
+import React, { useContext, useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import { Helmet } from "react-helmet-async";
+import CheckoutSteps from "../components/CheckoutSteps";
+import { Store } from "../Store";
+import { useNavigate } from "react-router-dom";
+
+export default function PaymentMethodScreen() {
+  const navigate = useNavigate();
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const {
+    cart: { shippingAddress, paymentMehod },
+  } = state;
+
+  const [paymentMehodName, setPaymentMethod] = useState(
+    paymentMehod || "Paypal"
+  );
+
+  useEffect(() => {
+    if (!shippingAddress.address) {
+      navigate("/shipping");
+    }
+  }, [shippingAddress, navigate]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    ctxDispatch({
+      type: "SAVE_PAYMENT_METHOD",
+      payload: paymentMehodName,
+    });
+    localStorage.setItem("payymentMethod", paymentMehodName);
+    navigate("/placeorder");
+  };
+
+  return (
+    <div>
+      <CheckoutSteps step1 step2 step3></CheckoutSteps>
+      <div className="container small-container">
+        <Helmet>
+          <title>Payment Method</title>
+        </Helmet>
+        <h1 className="my-3">Payment Method</h1>
+        <Form onSubmit={submitHandler}>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="Paypal"
+              label="Paypal"
+              value="Paypal"
+              checked={paymentMehodName === "Paypal"}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <Form.Check
+              type="radio"
+              id="Stripe"
+              label="Stripe"
+              value="Stripe"
+              checked={paymentMehodName === "Stripe"}
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <Button type="submit">Continue</Button>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+}
